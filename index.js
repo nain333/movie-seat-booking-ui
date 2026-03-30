9e29a6a810911caae1722d1d5bbbd3a8f8cf8aca
nain333 <hnain70@gmai.com>
2026-03-30 12:43:32 +0530

    set dynamic total price in summary

--- index.js
+++ index.js
@@ -50,35 +50,59 @@ for (let i = 0; i < row; i++) {
   }
 }
 // toggle seat selects on click
-seatGrid.addEventListener('click',(e)=>{
-    const seat =e.target;
-    if(!seat.classList.contains('seat')){
-        return;
-    }
-    if(seat.classList.contains('occupied')){
-        return;
-    }
-    seat.classList.toggle('selected');
 
-})
+seatGrid.addEventListener("click", (e) => {
+  const seat = e.target;
+
+  if (!seat.classList.contains("seat")) {
+    return;
+  }
+  if (seat.classList.contains("occupied")) {
+    return;
+  }
+  seat.classList.toggle("selected");
+  const selectedSeatCounter = seatGrid.querySelectorAll('.seat.selected').length;
+  const summarySeatsEl=document.querySelector('#count')
+  summarySeatsEl.textContent=selectedSeatCounter;
+  // total summary price
+  const select=document.querySelector('select');
+  console.log(select)
+  const option = select.options[select.selectedIndex];
+  const moviePrice=Number(option.dataset.price);
+  const totalPrice=moviePrice*selectedSeatCounter;
+  console.log(moviePrice)
+  console.log(option)
+  console.log(option)
+  
+
+  const summaryTotalPriceEl=document.querySelector('#total');
+  summaryTotalPriceEl.textContent=`₹ ${totalPrice}` 
+  
+
+  
+
+  
+  console.log(selectedSeatCounter);
+});
+
 const movies = [
-  { movieName: 'Flash', moviePrice: 150 },
-  { movieName: 'Superman', moviePrice: 250 },
-  { movieName: 'Batman', moviePrice: 200 },
-  { movieName: 'Spiderman', moviePrice: 180 },
-  { movieName: 'Iron Man', moviePrice: 220 },
-  { movieName: 'Thor', moviePrice: 210 },
-  { movieName: 'Hulk', moviePrice: 170 },
-  { movieName: 'Captain America', moviePrice: 230 },
-  { movieName: 'Doctor Strange', moviePrice: 240 },
-  { movieName: 'Black Panther', moviePrice: 260 }
+  { movieName: "Flash", moviePrice: 150 },
+  { movieName: "Superman", moviePrice: 250 },
+  { movieName: "Batman", moviePrice: 200 },
+  { movieName: "Spiderman", moviePrice: 180 },
+  { movieName: "Iron Man", moviePrice: 220 },
+  { movieName: "Thor", moviePrice: 210 },
+  { movieName: "Hulk", moviePrice: 170 },
+  { movieName: "Captain America", moviePrice: 230 },
+  { movieName: "Doctor Strange", moviePrice: 240 },
+  { movieName: "Black Panther", moviePrice: 260 },
 ];
-const selectMovieCon=document.querySelector('.movieSelect')
-const selectMovie=document.createElement('select');
+const selectMovieCon = document.querySelector(".movieSelect");
+const selectMovie = document.createElement("select");
 selectMovieCon.appendChild(selectMovie);
 // create options
-movies.forEach(movie => {
-  const option = document.createElement('option');
+movies.forEach((movie) => {
+  const option = document.createElement("option");
 
   option.value = movie.movieName;
   option.textContent = `${movie.movieName} - ₹${movie.moviePrice}`;
@@ -86,19 +110,18 @@ movies.forEach(movie => {
 
   selectMovie.appendChild(option);
 });
-selectMovie.addEventListener('change', (e) => {
+selectMovie.addEventListener("change", (e) => {
   const select = e.target;
   const option = select.options[select.selectedIndex];
+  
+  
 
-  document.querySelector('#movieName').textContent = option.value;
-  document.querySelector('#moviePrice').textContent = `₹ ${option.dataset.price}`;
+  document.querySelector("#movieName").textContent = option.value;
+  document.querySelector("#moviePrice").textContent =
+    `₹ ${option.dataset.price}`;
+    const selectedSeats=seatGrid.querySelectorAll('.seat.selected').length;
+    const  optionPrice=Number(option.dataset.price);
+    const totalPrice=document.querySelector('#total')
+    totalPrice.textContent=`₹ ${optionPrice*selectedSeats}`
 });
-
-
-
-
-
-
-
-
 
